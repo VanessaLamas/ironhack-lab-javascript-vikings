6548a49b3c3f8acd6ee8d45ebb4f7816346042a1
John Doe <johndoe@example.com>
2023-02-23 20:58:48 +0100

    receiveDamage function still pending

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,39 @@
+
+let damage = 10;
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack () {
+        return this.strength;
+    }
+    receiveDamage (damage) {
+        this.health = this.health - damage;
+        }
+    }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (displayName, health, strength){
+        displayName = "Roberto";
+        super(health, strength)
+    }
+    receiveDamage (damage){
+        this.health = this.health - damage;
+}   
+battleCry (){
+    return `Odin Owns You All!`;
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+}
 
-// War
-class War {}
+
+
